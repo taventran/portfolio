11adec29e2b90bfc8620933e5e43ecc8c83e567f
Taven Tran <taven@Tavens-MacBook-Pro.local>
2024-02-28 23:56:51 -0600

    first commit added text and animation

---
+++ src/components/navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./navbar.css";
+// "text-turqoise ml-[2%] mt-[2%] grid grid-cols-9 text-2xl font-bold "
+const Navbar = () => {
+  return <nav></nav>;
+};
+
+export default Navbar;
